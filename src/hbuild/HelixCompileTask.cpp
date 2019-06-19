b7214f3bde0ae57e1fab372290943c56b0da6bce
Steven M. Cherry <smc@hericus.com>
2019-06-19 11:01:09 -0600

    adding /nologo to the windows compile comannd line.

--- src/hbuild/HelixCompileTask.cpp
+++ src/hbuild/HelixCompileTask.cpp
@@ -165,7 +165,7 @@ twine HelixCompileTask::CC(const twine& tpl)
 	// ///////////////////////////////////////////////////////////////////////////////////
 	// 64-bit windows
 	// ///////////////////////////////////////////////////////////////////////////////////
-	return "cl.exe /std:c++14 -c -wd4251 -Zp8 -EHsc -O2 -D_WIN64 -DWIN64 -D_AMD64=1 -D__64BIT__ -DWIN32 -D_MT -D_DLL -DLINT_ARGS -MP8 -MD -W3 -D \"_CRT_SECURE_NO_DEPRECATE\" -D \"_CRT_NON_COMFORMING_SWPRINTFS\" -D CS_NO_LARGE_IDENTIFIERS -I " + tpl + "/include -I " + tpl + "/include/libxml2 -I . ";
+	return "cl.exe /nologo /std:c++14 -c -wd4251 -Zp8 -EHsc -O2 -D_WIN64 -DWIN64 -D_AMD64=1 -D__64BIT__ -DWIN32 -D_MT -D_DLL -DLINT_ARGS -MP8 -MD -W3 -D \"_CRT_SECURE_NO_DEPRECATE\" -D \"_CRT_NON_COMFORMING_SWPRINTFS\" -D CS_NO_LARGE_IDENTIFIERS -I " + tpl + "/include -I " + tpl + "/include/libxml2 -I . ";
 #	endif
 #elif __APPLE__
 	// ///////////////////////////////////////////////////////////////////////////////////
